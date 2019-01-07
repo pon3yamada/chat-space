05e6143e5bfab2ba4b610b38dffc00bb126e0fcc
pon3yamada <yamada.pon3@gmail.com>
2019-01-07 21:29:19 +0900

    Commit

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
@@ -14,5 +15,6 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
+    config.i18n.default_locale = :ja
   end
 end
